1501a6821e1dfb03cbf5f8a73b610c8430988de2
Sunnay <sunnay42@gmail.com>
2017-04-03 12:23:51 +0200

    First comit

---
+++ __init__.py
@@ -0,0 +1,58 @@
+from .main import xthor	
+
+def autoload():
+    return xthor()
+
+config = [{
+    'name': 'xthor',
+    'groups': [
+        {
+            'tab': 'searcher',
+            'list': 'torrent_providers',
+            'name': 'xthor',
+            'description': 'See <a href="https://xthor.bz/">XThor</a>',
+            'icon': 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAc5JREFUOI3tkk9IU3EAxz/vrbWeb+/NNdtmBS5kEHWwYfYHKulWCbtIh4i8JAYN6pRReKwMiooOElEGYkllUIgVTBhZRGHRKnBbaeGyOf+smshLeO/56xCMvHcJ+ty+l8/h+/3Cf6Q/w4VPYtvrkZk2c/DRzlQmbc3ajiKKnvvpC05MBcIfWb/5PTJ9RKWZRYJoUkRS77Ld+eL8Os/YMOM3z8PEKI1HY0xNFngaH6KuPsKHOShaNljSArUNLVxsuP5bc3zwTfuDpKB2n6CpQ0C12NIYE32js6L9dkK0nOkUL6dtUVW3W1xKpMX+1rOCrbEXADLAhuzA2ubtIfaEVDBtwMmRE8foudFNua5x9VQXd/oHKFNU4re6WOHR4cd0uCRI9vRa8VfDPLx3DZxOmtoO4xSCTRtrUL1eMDIEdRVddaNrOj6tDITDLAmg8DZvLyN66CQ4ZNLjBRLPhviay2MYBlBBld+Dx1OOoigskSQIrMyUStz7WKh3Tzf3OueKu8yaerBMiN+nsno1a1b5eZ7Konl9GO7laEtdVLqVsW8uf2yy82D/ohlb04LL5zquzAfDO8h/XkB2fEd25fAGvqBWpAhFnnBAGvm7T/r3+QVQzKvI28pwFwAAAABJRU5ErkJggg==',
+            'wizard': True,
+            'options': [
+                {
+                    'name': 'enabled',
+                    'type': 'enabler',
+                    'default': False,
+                },
+                {
+                    'name': 'passkey',
+                    'default': '',
+                },
+                {
+                    'name': 'ignore_year',
+                    'label': 'ignore year',
+                    'default': 0,
+                    'type': 'bool',
+                    'description': 'Will ignore the year in the search results',
+                },
+                {
+                    'name': 'multi',
+                    'label': 'ignore lang in multi',
+                    'default': 0,
+                    'type': 'bool',
+                    'description': 'Will ignore "french" and "truefrench" if "multi" is detected',
+
+                },
+                {
+                    'name': 'seed_ratio',
+                    'label': 'Seed ratio',
+                    'type': 'float',
+                    'default': 1,
+                    'description': 'Will not be (re)moved until this seed ratio is met.',
+                },
+                {
+                    'name': 'seed_time',
+                    'label': 'Seed time',
+                    'type': 'int',
+                    'default': 40,
+                    'description': 'Will not be (re)moved until this seed time (in hours) is met.',
+                }
+            ],
+        },
+    ],
+}]
